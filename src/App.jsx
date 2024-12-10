5838edd2978d612c0ad86eb4d68020188349e79d
kupz <kupzsensei@gmail.com>
2024-12-10 21:29:56 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StudentList from "./component/student-list";
 
 function App() {
@@ -6,6 +6,13 @@ function App() {
     { name: "Benj", section: "Section1", course: "BSIT" },
     { name: "Grace", section: "Section2", course: "BSIT" },
   ]);
+
+  const dialogNameRef = useRef(null);
+  const dialogCourseRef = useRef(null);
+  const dialogSectionRef = useRef(null);
+
+  const [dialog, setDialog] = useState(false);
+  const [editData, setEditData] = useState({});
 
   const [searchFilter, setSearchFilter] = useState("");
 
@@ -37,6 +44,14 @@ function App() {
     setSearchFilter(e.target.value);
   };
 
+  useEffect(() => {
+    if (editData.name) {
+      dialogNameRef.current.value = editData?.name;
+      dialogCourseRef.current.value = editData?.course;
+      dialogSectionRef.current.value = editData?.section;
+    }
+  }, [editData]);
+
   return (
     <main className="w-screen h-screen bg-green-300 flex flex-col gap-5">
       <div className="flex gap-5 p-5 items-center">
@@ -57,7 +72,67 @@ function App() {
         </div>
       </div>
 
-      <StudentList data={filteredItem} updateStudents={setStudents} />
+      <StudentList
+        data={filteredItem}
+        updateStudents={setStudents}
+        dialog={dialog}
+        setDialog={setDialog}
+        setEditData={setEditData}
+      />
+
+      {dialog && (
+        <div className="w-screen h-screen bg-black/50 absolute top-0 left-0 flex justify-center items-center">
+          <div className="flex flex-col p-5 bg-gray-200 gap-3">
+            <input
+              type="text"
+              placeholder="name"
+              className="p-2"
+              ref={dialogNameRef}
+            />
+            <input
+              type="text"
+              placeholder="course"
+              className="p-2"
+              ref={dialogCourseRef}
+            />
+            <input
+              type="text"
+              placeholder="section"
+              className="p-2"
+              ref={dialogSectionRef}
+            />
+            <button
+              className="bg-yellow-200 p-2"
+              onClick={() => {
+                setStudents((prev) => {
+                  const updated = students.map((row) => {
+                    if (row.name === editData.name) {
+                      return {
+                        name: dialogNameRef.current.value,
+                        course: dialogCourseRef.current.value,
+                        section: dialogSectionRef.current.value,
+                      };
+                    }
+                    return row;
+                  });
+                  return updated;
+                });
+                setDialog(false);
+              }}
+            >
+              update
+            </button>
+            <button
+              className="bg-gray-500 p-2"
+              onClick={() => {
+                setDialog(false);
+              }}
+            >
+              cancel
+            </button>
+          </div>
+        </div>
+      )}
     </main>
   );
 }
--- src/component/student-list.jsx
+++ src/component/student-list.jsx
@@ -1,4 +1,9 @@
-export default function StudentList({ data, updateStudents }) {
+export default function StudentList({
+  data,
+  updateStudents,
+  setDialog,
+  setEditData,
+}) {
   return (
     <table className="table table-auto w-full border-spacing-2 border-separate">
       <thead>
@@ -15,7 +20,16 @@ export default function StudentList({ data, updateStudents }) {
             <td>{student.name}</td>
             <td>{student.course}</td>
             <td>{student.section}</td>
-            <td>
+            <td className="flex gap-3 items-center justify-center">
+              <button
+                className="bg-yellow-500 text-white p-2"
+                onClick={() => {
+                  setDialog(true);
+                  setEditData(student);
+                }}
+              >
+                edit
+              </button>
               <button
                 className="bg-red-500 text-white p-2"
                 onClick={() => {
